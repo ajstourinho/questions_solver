f44cb3c3824c3f80e9f3f0668f1f5f3f03313472
ajstourinho <ajstourinho@gmail.com>
2024-08-24 19:36:26 -0300

    Add handleSubmit function

--- frontend/src/components/SolveButton/SolveButton.tsx
+++ frontend/src/components/SolveButton/SolveButton.tsx
@@ -1,8 +1,42 @@
 import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+
+import axiosInstance from "../../axios/axiosInstance";
 
 export default function SolveButton() {
+  const files = useSelector((state: RootState) => state.filesSlice.files);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (files.length === 0) {
+      alert("Por favor, selecione 1 arquivo PDF primeiro.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", files[0]);
+
+    try {
+      const response = await axiosInstance.post(
+        "/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("File uploaded successfully", response.data);
+    } catch (error) {
+      console.error("There was an error uploading the file!", error);
+    }
+  };
+
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={handleSubmit}>
       RESOLVER!
     </Button>
   );
