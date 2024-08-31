607abcad0de73ccffff586e1b4717b94adb3266f
ajstourinho <ajstourinho@gmail.com>
2024-08-30 22:01:59 -0300

    Implement Modal handling

--- frontend/src/components/SolveButton/SolveButton.tsx
+++ frontend/src/components/SolveButton/SolveButton.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Button } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import { RootState } from "../../store/store";
-
+import CustomModal from "../Modal/Modal";
 import axiosInstance from "../../axios/axiosInstance";
 
 export default function SolveButton() {
   const files = useSelector((state: RootState) => state.filesSlice.files);
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleCloseModal = () => setOpenModal(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -14,6 +19,8 @@ export default function SolveButton() {
       alert("Por favor, selecione 1 arquivo PDF primeiro.");
       return;
     }
+
+    setOpenModal(true);
 
     const formData = new FormData();
     formData.append("file", files[0]);
@@ -36,8 +43,14 @@ export default function SolveButton() {
   };
 
   return (
-    <Button variant="contained" color="primary" onClick={handleSubmit}>
-      RESOLVER!
-    </Button>
+    <>
+      <Button variant="contained" color="primary" onClick={handleSubmit}>
+        RESOLVER!
+      </Button>
+      <CustomModal open={openModal} onClose={handleCloseModal}>
+        <h2 id="simple-modal-title">Solução</h2>
+        <p id="simple-modal-description">Aqui vai a solução do problema.</p>
+      </CustomModal>
+    </>
   );
 }
