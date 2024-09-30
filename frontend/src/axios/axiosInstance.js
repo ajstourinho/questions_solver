df79e8096c42c161f865b6833bad611cd770165c
ajstourinho <ajstourinho@gmail.com>
2024-09-30 00:35:36 -0300

    Correct HTTPS /api route

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.REACT_APP_ENV === "production"
-  ? "/api/" : "http://localhost:5000/api/";
+    ? "api.iloveprovaantiga.com.br/api/" //https
+    : "http://localhost:5000/api/";
 
 const axiosInstance = axios.create({  
   baseURL: BASE_URL,
