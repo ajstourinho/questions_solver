b5e66e0f6a33b1dac9d0a151556e20864fd84ff9
Andre Luiz Fernandes <andreluiz.lf11@hotmail.com>
2024-12-29 19:26:03 -0300

    function to load coupons added

--- backend/coupons/couponsFile.py
+++ backend/coupons/couponsFile.py
@@ -1,3 +1,11 @@
-coupons = {
-  'medusp110': 0.05
-}
+import csv
+
+def loadCoupons(emptyDict):
+  try:
+    with open('coupons.csv', 'r') as file:
+      csv_reader = csv.DictReader(file)
+      for row in csv_reader:
+         emptyDict[row['COUPON']] = float(row['MULTIPLIER'])
+  except Exception as e:
+    print('There was a problem loading the coupon file: ' + str(e))
+    
