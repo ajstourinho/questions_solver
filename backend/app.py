7af4cb5c24fe3fe93a16eb8460a59dc5279bbbf3
Andre Luiz Fernandes <andreluiz.lf11@hotmail.com>
2025-01-12 11:12:06 -0300

    old gpt endpoint in app.py removed

--- backend/app.py
+++ backend/app.py
@@ -87,52 +87,6 @@ def upload_file():
         loggerFlask.info("Upload done of file:" + str(file_path))
         return jsonify({'message': 'File uploaded successfully', 'file_path': file_path}), 200
     
-
-# @app.route('/api/gpt_solver', methods=['POST'])
-# def gpt_solver():
-#     """
-#     Endpoint to activate GPT API on previously uploaded file, after having confirmed the payment.
-#     """
-
-#     # Consider the filename and pageCount are being sent in the request body
-#     data = request.get_json()
-#     pdf_filename = data.get('filename')
-#     pdf_pageCount = int(data.get('pageCount'))
-
-#     # Define the path of uploaded PDF file, based on the filename
-#     pdf_directory = FOLDER_UPLOADED_FILES
-#     pdf_path = os.path.join(pdf_directory, pdf_filename)
-
-#     # Define file basename (without extension)
-#     file_basename, _ = os.path.splitext(pdf_filename)
-
-#     # Generate iterable of PDF pages as images
-#     pdf_pages_as_imgs = convert_from_path(pdf_path)
-
-#     # Iterate images
-#     for i, image in enumerate(pdf_pages_as_imgs):
-#         # Define image basename (without extension)
-#         image_basename = f"{file_basename}_page_{i+1}" # Without extension
-
-#         # Save image as .png
-#         gpt_api.save_img(image_basename, image)
-
-#         # Call GPT API to generate JSON from processed image
-#         gpt_api.generate_json(image_basename)
-
-#     # Generate PDF from JSONs
-#     gpt_api.generate_pdf_from_jsons(file_basename)
-
-#     # Send the PDF file as a downloadable response
-#     pdf_path_temp = os.path.join(FOLDER_OUTPUT_2_PDFS, file_basename + "_resolvida.pdf") 
-#     return send_file(
-#         pdf_path_temp,
-#         mimetype='application/pdf',  # Specify the MIME type as PDF
-#         as_attachment=True,  # Force the download
-#         download_name="prova_resolvida.pdf"  # Set the filename for the download
-#     )
-
-
 @app.route('/api/cob', methods=['POST'])
 def pix_new_cob():
     """
